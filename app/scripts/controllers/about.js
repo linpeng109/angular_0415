2d9c41e6eb09c66e4706184a43e6b43a07a98a4a
linpeng109 <linpeng109@aliyun.com>
2016-04-15 17:29:45 +0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angular0415App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angular0415App
+ */
+angular.module('angular0415App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angular0415App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angular0415App
+ */
+angular.module('angular0415App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

ec3012bb7003c0aa2774e485ec809876e21895c1
linpeng109 <linpeng109@aliyun.com>
2016-04-19 16:00:29 +0800

    使用get方式实现http跨域访问

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -8,10 +8,11 @@
  * Controller of the angular0415App
  */
 angular.module('angular0415App')
-  .controller('ListCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ListCtrl', function ($scope, $http) {
+
+
+    $http.get('http://localhost:3000/findByPage?callback=JSON_CALLBACK').success(function (data) {
+      console.log(data);
+      $scope.users = data;
+    });
   });

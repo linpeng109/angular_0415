bbca0291ddbfdf03caf2147ab0ab071423e90aec
linpeng109 <linpeng109@aliyun.com>
2016-08-22 16:09:16 +0800

    20160822

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -10,9 +10,10 @@
 angular.module('angular0415App')
   .controller('ListCtrl', function ($scope, $http) {
     $http
-      .jsonp('http://192.168.0.100:3000/list/findAll?callback=JSON_CALLBACK')
+      .jsonp('http://192.168.0.103:3000/list/findByPage?pageSize=10&pageNum=1&callback=JSON_CALLBACK')
       .success(function (data) {
         $scope.users = data;
+        $scope.itemsByPage = 15;
       });
   });
 

50d230efc7eb5ea3ad28112c6476039d4c3347a4
linpeng109 <linpeng109@aliyun.com>
2016-08-25 16:21:12 +0800

    :20160825

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,10 +8,19 @@
  * Controller of the angular0415App
  */
 angular.module('angular0415App')
-  .controller('LoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('LoginCtrl', function ($scope, $http) {
+
+    $scope.passportLocalLogin = function () {
+      var url = 'http://192.168.0.103:3000/passportLocal/login';
+      var params = {
+        username: $scope.username,
+        password: $scope.password,
+        callback: 'JSON_CALLBACK'
+      };
+      $http
+        .jsonp(url, {params: params})
+        .success(function (data) {
+          $scope.result = JSON.stringify(data.result);
+        });
+    };
   });
